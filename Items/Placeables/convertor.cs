27eeba0be2e46af248fe1fe9df3bd3c50c6e02ce
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-06-16 13:23:23 -0500

    Began work on the converter

---
+++ Items/Placeables/convertor.cs
@@ -0,0 +1,37 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace prismmod.Items.Placeables
+{
+    public class Converter : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("This is a modded workbench.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 14;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.value = 150;
+            item.createTile = mod.TileType("Converter");
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.WorkBench);
+            //recipe.AddIngredient(mod.ItemType("ExampleBlock"), 10);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
