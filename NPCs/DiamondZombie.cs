8bdcf92f974cfa79b05f163a2499eef13dae13f4
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-06-27 23:18:58 -0500

    Added DiamondZombie Code and a plethora of mostly useless Sprites for it

--- NPCs/DiamondZombie.cs
+++ NPCs/DiamondZombie.cs
@@ -5,7 +5,39 @@ using Microsoft.Xna.Framework;
 
 namespace prismmod.NPCs
 {
-    class DiamondZombie : ModNPCs
+    class DiamondZombie : ModNPC
     {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Diamond Zombie");
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Zombie];
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 20;
+            npc.height = 40;
+            animationType = NPCID.Zombie;
+            npc.aiStyle = 3;
+            npc.lifeMax = 75;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.value = 0.75f;
+
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldNightMonster.Chance*0.09f;
+        }
+
+        public override void NPCLoot()
+        {
+            Item.NewItem(npc.getRect(), ItemID.Diamond, Main.rand.Next(3, 6));
+        }
+
+
     }
+
+
 }
