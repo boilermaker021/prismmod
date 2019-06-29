18c5e02fcc985b1903ca370a9b883067377c3357
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-06-28 22:44:06 -0500

    Reduced Trapping Edge effectiveness against bosses

--- Buffs/Freezing.cs
+++ Buffs/Freezing.cs
@@ -10,8 +10,16 @@ namespace prismmod.Buffs
 
 		public override void Update(NPC target, ref int buffIndex)
 		{
-            target.velocity.Y = 9.8f;
-            target.velocity.X = 0f;
+            if (target.boss == false)
+            {
+                target.velocity.Y = 9.8f;
+                target.velocity.X = 0f;
+            }
+            else
+            {
+                target.velocity *= 0.8f;
+
+            }
 		}
 	}
 }
