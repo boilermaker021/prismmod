c51c269d4960b7979af982e315c0865d2bd6d35d
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-07-09 00:53:43 -0500

    Create Edgar.cs

---
+++ NPCs/Edgar.cs
@@ -0,0 +1,66 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace prismmod.NPCs
+{
+    class Edgar : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Edgar");
+            Main.npcFrameCount[npc.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 18;
+            npc.height = 34;
+            npc.aiStyle = 22;
+            npc.lifeMax = 40;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.value = 0.75f;
+            npc.knockBackResist = 0.5f;
+            npc.noTileCollide = true;
+            npc.noGravity = true;
+            npc.damage = 10;
+
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldNightMonster.Chance * 0.1f;
+        }
+
+
+        public override void PostDraw(SpriteBatch spriteBatch, Color drawColor)
+        {
+            Texture2D texture = mod.GetTexture("NPCs/Edgar_Glowmask");
+            spriteBatch.Draw
+            (
+                texture,
+                new Vector2
+                (
+                    npc.position.X - Main.screenPosition.X + npc.width * 0.5f,
+                    npc.position.Y - Main.screenPosition.Y + npc.height - texture.Height * 0.5f + 2f
+                ),
+                new Rectangle(0, 0, texture.Width, texture.Height),
+                Color.White,
+                npc.rotation,
+                texture.Size()*0.5f,
+                npc.scale,
+                SpriteEffects.None,
+                0f
+            );
+        }
+
+        public override void AI()
+        {
+            
+            
+        }
+    }
+}
