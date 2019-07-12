6107b18c5ce93674e48f0a41098d8935afe39b89
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-07-11 23:52:52 -0500

    Added variables to increase flamethrower range and damage

--- PrismPlayer.cs
+++ PrismPlayer.cs
@@ -16,6 +16,8 @@ namespace prismmod
         public float noAmmoUseChance = 0;
         public float IncreaseBulletSpeed = 0;
         public int timesBounced = 0;
+        public float flamerDamageIncrease = 1f;
+        public float flamerSpeedIncrease = 0f;
 
         
         public override bool ConsumeAmmo(Item weapon, Item ammo)
@@ -29,8 +31,26 @@ namespace prismmod
             noAmmoUseChance = 0f;
             return true;
         }
+        public override void ResetEffects()
+        {
+            flamerDamageIncrease = 1f;
+            flamerSpeedIncrease = 1f;
+        }
 
-        
+        public override bool Shoot(Item item, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (item.useAmmo == AmmoID.Gel)
+            {
+                damage = (int)((float)damage * flamerDamageIncrease);
+                speedY *= flamerSpeedIncrease;
+                speedX *= flamerSpeedIncrease;
+
+            }
+            return true;
+        }
+
+
+
 
 
     }
