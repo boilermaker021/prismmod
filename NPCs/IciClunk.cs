0e1ca27307f43f2ad80c3bfcca2b0be03bcd493f
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-07-16 20:38:28 -0500

    Added nonresponsive Iciclunk (No AI)

---
+++ NPCs/IciClunk.cs
@@ -0,0 +1,58 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace prismmod.NPCs
+{
+    class Iciclunk : ModNPC
+    {
+
+        Player player;
+        private float speed;
+
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Iciclunk");
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[5];//will change
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width =18;
+            npc.height = 34;
+            animationType = 0;
+            npc.aiStyle = 0;
+            npc.lifeMax = 75;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.value = 0.75f;
+            npc.knockBackResist= 0.5f;
+
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldNightMonster.Chance*0.1f;
+        }
+
+        public override void NPCLoot()
+        {
+            Item.NewItem(npc.getRect(), mod.ItemType("IciclunkHorn"), Main.rand.Next(3, 6));
+        }
+
+        public void Target()
+        {
+            player = Main.player[npc.target];
+        }
+
+        public void AI()
+        {
+
+        }
+
+    }
+
+
+}
