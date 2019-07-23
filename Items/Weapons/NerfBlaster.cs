02b9801b3fbfa7dc92016dcb362af196b4f339a7
Jellyman0613 <Jellyman0613yt@gmail.com>
2019-07-23 11:24:20 -0500

    Nerf Gun

---
+++ Items/Weapons/NerfBlaster.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace prismmod.Items.Weapons
+{
+    public class Nerfblaster : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nerf Gun");
+            Tooltip.SetDefault("It's Nerf or Nathan");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 69420;
+            item.ranged = true;
+            item.width = 18;
+            item.height = 12;
+            item.useTime = 30;
+            item.useAnimation = 30;
+            item.useStyle = 5;
+            item.noMelee = true; //so the item's animation doesn't do damage
+            item.knockBack = 69;
+            item.value = 10000;
+            item.rare = 2;
+            item.UseSound = SoundID.Item11;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("Nerfdart");
+            item.shootSpeed = 6f;
+            item.useAmmo = mod.ItemType("Nerfdart");
+            item.scale = 1.5f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DynastyWood, 5);
+            recipe.AddIngredient(ItemID.IronBar, 10);
+            recipe.AddIngredient(ItemID.GrayPressurePlate, 1);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-8, 0);
+        }
+    }
+}
---
+++ Projectiles/Nerfdart.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace prismmod.Projectiles
+{
+    public class Nerfdart : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.Name = "Nerf Dart";
+            projectile.width = 6;
+            projectile.height = 2;
+            projectile.aiStyle = 1;
+            projectile.friendly = true;
+            projectile.hostile = false;
+            projectile.ranged = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 600;
+            projectile.alpha = 255;
+            projectile.light = 0f;
+            projectile.ignoreWater = false;
+            projectile.tileCollide = true;
+            projectile.extraUpdates = 1;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+            aiType = ProjectileID.Bullet;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            //If collide with tile, reduce the penetrate.
+            //So the projectile can reflect at most 5 times
+            projectile.penetrate--;
+            if (projectile.penetrate <= 0)
+            {
+                projectile.Kill();
+            }
+            else
+            {
+                if (projectile.velocity.X != oldVelocity.X)
+                {
+                    projectile.velocity.X = -oldVelocity.X;
+                }
+                if (projectile.velocity.Y != oldVelocity.Y)
+                {
+                    projectile.velocity.Y = -oldVelocity.Y;
+                }
+                Main.PlaySound(SoundID.Item10, projectile.position);
+            }
+            return false;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
+            for (int k = 0; k < projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+                Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+                spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+            }
+            return true;
+        }
+    }
+}
