ccb734e65dbb2a280693b58ff2ef27d49185a6be
Jellyman0613 <Jellyman0613yt@gmail.com>
2019-07-23 07:57:15 -0500

    Sandy Armor
    Sandy Armor (face is invisible glitch. will fix later)

---
+++ Items/Armor/sandylegs.cs
@@ -0,0 +1,36 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace prismmod.Items.Armor
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class Sandylegs : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sandy Boots");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 7;
+            item.height = 6;
+            item.rare = 1;
+            item.defense = 69;
+            
+        }
+        
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType("SpecialCloth"), 4);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+
+        }
+    }
+}
