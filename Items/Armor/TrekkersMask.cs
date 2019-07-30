1186f16a84596f624dadb71c12b8b2e9f653b0ce
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-07-30 01:47:03 -0500

    Increased chance of arrows to freeze enemies

--- Items/Armor/TrekkersMask.cs
+++ Items/Armor/TrekkersMask.cs
@@ -35,9 +35,9 @@ namespace prismmod.Items.Armor
 
         public override void UpdateArmorSet(Player player)
         {
-            player.setBonus = "Immunity to being Chilled, 10% chance for bows to freeze enemies";
+            player.setBonus = "Immunity to being Chilled, 25% chance for bows to freeze enemies";
             player.buffImmune[BuffID.Chilled] = true;
-            player.GetModPlayer<PrismPlayer>().arrowsFreezeEnemies += 0.10f;
+            player.GetModPlayer<PrismPlayer>().arrowsFreezeEnemies += 0.25f;
         }
 
         public override void AddRecipes()
