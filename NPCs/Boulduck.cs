c6a31418b8a2c3ec2c6c62fb2765b6ffcaa669b5
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-07-30 23:13:56 -0500

    Create Boulduck.cs

---
+++ NPCs/Boulduck.cs
@@ -0,0 +1,46 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace prismmod.NPCs
+{
+    class Boulduck : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Boulduck");
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Unicorn];
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width =74;
+            npc.height = 50;
+            animationType = NPCID.Zombie;
+            npc.aiStyle = 26;
+            npc.lifeMax = 75;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.value = 0.75f;
+            npc.knockBackResist = 0.5f;
+            npc.damage = 25;
+            
+
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldNightMonster.Chance*0.1f;
+        }
+
+        public override void NPCLoot()
+        {
+            Item.NewItem(npc.getRect(), mod.ItemType("BoulduckFeather"), Main.rand.Next(3, 6));
+        }
+
+
+    }
+
+
+}
