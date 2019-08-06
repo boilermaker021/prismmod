06faec02cbe648f43d73722c9555b606ebb54361
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-08-06 14:44:25 -0500

    Created PrismGlobalItem for modifying Vanilla items

---
+++ PrismGlobalItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace prismmod
+{
+    class PrismGlobalItem : GlobalNPC
+    {
+        
+
+    }
+}
