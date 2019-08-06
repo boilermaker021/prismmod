9dc278f109f72e95b87bc209aa091c0327c1ce3a
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-08-06 12:46:11 -0500

    Added Kanaya's Chainsaw

---
+++ Items/Tools/KanayasChainsaw.cs
@@ -0,0 +1,58 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace prismmod.Items.Tools
+{
+	public class KanayasChainsaw : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Kanaya's Lipstick");
+			Tooltip.SetDefault("Think of something creative");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = 20;
+			item.melee = true;
+			item.width = 54;
+			item.height = 14;
+			item.useTime = 7;
+			item.useAnimation = 25;
+            item.channel = true;
+            item.noUseGraphic = true;
+			item.useStyle = 5;
+            item.noMelee = true;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item22;
+			item.autoReuse = true;
+            item.axe = 165;
+            item.shoot = mod.ProjectileType("KanayasChainsawProjectile");
+            item.shootSpeed = 40f;
+		}
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            
+            spriteBatch.Draw(mod.GetTexture("Items/Tools/KanayasLipstick"),
+                  new Rectangle((int)position.X+10, (int)position.Y+10, (int)(14 * scale), (int)(36 * scale)),
+                  drawColor);
+            return false; 
+        }
+
+        /*public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddTile(TileID.WorkBenches);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}*/
+
+
+    }
+}
---
+++ Items/Tools/KanayasChainsawProjectile.cs
@@ -0,0 +1,46 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace prismmod.Items.Tools
+{
+	public class KanayasChainsawProjectile : ModProjectile
+	{
+		
+		public override void SetDefaults()
+		{
+            projectile.Name = "Kanaya's Chainsaw";
+            projectile.width = 22;
+            projectile.height = 22;
+            projectile.aiStyle = 20;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+            projectile.tileCollide = false;
+            projectile.hide = true;
+            projectile.ownerHitCheck = true;
+            projectile.melee = true;
+		}
+
+        /*public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            
+            spriteBatch.Draw(mod.GetTexture("Items/Tools/KanayasLipstick"),
+                  new Rectangle((int)position.X, (int)position.Y, (int)(14 * scale), (int)(36 * scale)),
+                  drawColor);
+            return false; 
+        }*/
+
+        /*public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddTile(TileID.WorkBenches);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}*/
+
+
+    }
+}
