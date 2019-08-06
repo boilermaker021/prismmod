47d4c9bab1a01bc38bbb190dfe8d9b9aa7fde8c2
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-08-06 13:36:54 -0500

    Created the Pshoooooes

---
+++ Items/Accessories/Pshoes.cs
@@ -0,0 +1,40 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace prismmod.Items.Accessories
+{
+	public class Pshoes : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Pshooooes");
+			Tooltip.SetDefault("Don't you dare visit your denizen early");
+		}
+		public override void SetDefaults()
+		{
+            item.accessory = true;
+		}
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.canRocket = true;
+            player.rocketBoots = 1;
+            //player.rocketTime = 240;
+            player.rocketTimeMax = 24;
+        }
+
+
+        public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.TungstenBar, 5);
+            recipe.AddIngredient(ItemID.GoldBar, 5);
+            recipe.AddIngredient(ItemID.Wood, 8);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
