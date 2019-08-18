4d0497dfc7b8a73c176cfda2f330c6c2c7089559
NicholasBird <42478497+NicholasBird@users.noreply.github.com>
2019-08-17 23:33:31 -0500

    Update RainBow.cs

--- Items/Weapons/RainBow.cs
+++ Items/Weapons/RainBow.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace prismmod.Items.Weapons.Items
+namespace prismmod.Weapons.Items
 {
     public class RainBow : ModItem
     {
