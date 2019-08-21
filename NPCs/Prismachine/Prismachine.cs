91e20744be89b7d86543176cb2516fb2c5817710
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-08-21 20:10:09 +0000

    Added AI_States

--- NPCs/Prismachine/Prismachine.cs
+++ NPCs/Prismachine/Prismachine.cs
@@ -36,6 +36,51 @@ namespace prismmod.NPCs.Prismachine
             //Item.NewItem(npc.getRect(), ItemID.Diamond, Main.rand.Next(3, 6));
         }
 
+
+        private const int AI_State_Slot = 0;
+        private const int AI_Timer_Slot = 1;
+        private const int AI_Attack_Slot_Element1 = 2;
+        private const int AI_Attack_Slot_Element2 = 3;
+        private const int AI_Attack_Slot_Element3 = 4;
+        private const int AI_Attack_Slot_Element4 = 5;
+
+        public float AI_State
+        {
+            get => npc.ai[AI_State_Slot];
+            set => npc.ai[AI_State_Slot] = value;
+
+        }
+
+        public float AI_Timer {
+            get => npc.ai[AI_Timer_Slot];
+            set => npc.ai[AI_Timer_Slot] = value;
+
+        }
+
+        public float AI_Attack_Element1 {
+            get => npc.ai[AI_Attack_Slot_Element1];
+            set => npc.ai[AI_Attack_Slot_Element1] = value;
+
+        }
+
+        public float AI_Attack_Element2 {
+            get => npc.ai[AI_Attack_Slot_Element2];
+            set => npc.ai[AI_Attack_Slot_Element2] = value;
+
+        }
+
+        public float AI_Attack_Element3 {
+            get => npc.ai[AI_Attack_Slot_Element3];
+            set => npc.ai[AI_Attack_Slot_Element3] = value;
+
+        }
+
+        public float AI_Attack_Element4 {
+            get => npc.ai[AI_Attack_Slot_Element4];
+            set => npc.ai[AI_Attack_Slot_Element4] = value;
+
+        }
+
         public override void AI()
         {
             //start working on AI
