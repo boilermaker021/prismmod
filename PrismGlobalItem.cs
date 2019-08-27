b1173996a48c4c847c98b93140fb9b715aa5b448
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-08-27 00:31:04 -0500

    Revert "Added basics for Ninja Armor revamp"
    This reverts commit ed33ad3d141541fe05a4c72603f51c6127136609.

--- PrismGlobalItem.cs
+++ PrismGlobalItem.cs
@@ -9,45 +9,16 @@ namespace prismmod
 {
     class PrismGlobalItem : GlobalItem
     {
-
-        public override void SetDefaults(Item item)
+        public override void UpdateEquip(Item item, Player player)
         {
-            if (item.type == ItemID.NinjaHood || item.type == ItemID.NinjaPants)
+            if (item.type == ItemID.NinjaHood||item.type == ItemID.NinjaPants)
             {
                 item.defense = 1;
+                player.GetModPlayer<PrismPlayer>().reducedContactDamage *= 1.10f;
             }
             if (item.type == ItemID.NinjaShirt)
             {
                 item.defense = 2;
-            }
-        }
-
-        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
-        {
-            if (item.type == ItemID.NinjaHood || item.type == ItemID.NinjaPants || item.type == ItemID.NinjaShirt)
-            { 
-                tooltips[3] = new TooltipLine(mod, "Tooltip4", "-10% Contact Damage \nDoes some other shit too, haven't decided yet");
-
-            }
-        }
-
-        public override void UpdateArmorSet(Player player, string set)
-        {
-            if (set == "NinjaArmor")
-            {
-                player.setBonus = "Allows you to dash in any direction //MUST BE IMPLEMENTED AHHHHHHHHH";
-                
-            }
-        }   
-
-        public override void UpdateEquip(Item item, Player player)
-        {
-            if (item.type == ItemID.NinjaHood||item.type == ItemID.NinjaPants|| item.type == ItemID.NinjaShirt)
-            {
-                if (item.type == ItemID.NinjaHood)
-                {
-                    player.thrownVelocity -= 0.15f;
-                }
                 player.GetModPlayer<PrismPlayer>().reducedContactDamage *= 1.10f;
             }
         }
