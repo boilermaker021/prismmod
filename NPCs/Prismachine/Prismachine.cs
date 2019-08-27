56245d0e64e7b3852f6dc64fa13ecd81ff34b72b
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-08-26 23:34:48 -0500

    Prismachine now moves.... very often

--- NPCs/Prismachine/Prismachine.cs
+++ NPCs/Prismachine/Prismachine.cs
@@ -10,7 +10,7 @@ namespace prismmod.NPCs.Prismachine
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Prismachine");
+            DisplayName.SetDefault("The Prismachine");
             Main.npcFrameCount[npc.type] = 9;
         }
 
@@ -28,6 +28,8 @@ namespace prismmod.NPCs.Prismachine
             npc.knockBackResist = 1f;
             npc.damage = 1000; //lots of damage, should not get close to it, bceeause it should not get close to you
             // npc.immune = false;
+            npc.noGravity = true;
+            npc.noTileCollide = true;
         }
 
 
@@ -46,7 +48,9 @@ namespace prismmod.NPCs.Prismachine
         private const int AI_Attack_Slot_Element2 = 3;
         private const int AI_Attack_Slot_Element3 = 4;
         private const int AI_Attack_Slot_Element4 = 5;
-        private const int AI_Orbs_Spawned_Slot = 6;
+        private bool orbsSpawned = false;
+        private bool[] Attacks_Enabled = { false, false, false, false };
+        private Random generator = new Random();
 
 
 
@@ -63,62 +67,67 @@ namespace prismmod.NPCs.Prismachine
 
         }
 
-        public float AI_Attack_Element1 {//use 0 to signify enabled attacks, and anything else to signify enabled attacks
-            get => npc.ai[AI_Attack_Slot_Element1];
-            set => npc.ai[AI_Attack_Slot_Element1] = value;
+        public bool AI_Attack_Element1 {//use 0 to signify enabled attacks, and anything else to signify enabled attacks
+            get => Attacks_Enabled[0];
+            set => Attacks_Enabled[0] = value;
 
         }
 
-        public float AI_Attack_Element2 {
-            get => npc.ai[AI_Attack_Slot_Element2];
-            set => npc.ai[AI_Attack_Slot_Element2] = value;
+        public bool Attack_Element1
+        {//use 0 to signify enabled attacks, and anything else to signify enabled attacks
+            get => Attacks_Enabled[0];
+            set => Attacks_Enabled[0] = value;
 
         }
 
-        public float AI_Attack_Element3 {
-            get => npc.ai[AI_Attack_Slot_Element3];
-            set => npc.ai[AI_Attack_Slot_Element3] = value;
+        public bool Attack_Element2
+        {//use 0 to signify enabled attacks, and anything else to signify enabled attacks
+            get => Attacks_Enabled[1];
+            set => Attacks_Enabled[1] = value;
 
         }
 
-        public float AI_Attack_Element4 {
-            get => npc.ai[AI_Attack_Slot_Element4];
-            set => npc.ai[AI_Attack_Slot_Element4] = value;
+        public bool Attack_Element3
+        {//use 0 to signify enabled attacks, and anything else to signify enabled attacks
+            get => Attacks_Enabled[2];
+            set => Attacks_Enabled[2] = value;
 
         }
 
-        public float orbsSpawned{
-            get=> npc.ai[AI_Orbs_Spawned_Slot];
-            set=> npc.ai[AI_Orbs_Spawned_Slot] = value;
+        public bool Attack_Element4
+        {//use 0 to signify enabled attacks, and anything else to signify enabled attacks
+            get => Attacks_Enabled[3];
+            set => Attacks_Enabled[3] = value;
 
         }
 
         public override void AI()
         {
 
-            if(orbsSpawned!=0)
+            if(!orbsSpawned&&Main.netMode!=1)
             {
                 //orb spawn code
 
             }
 
-            bool move = (Main.player[npc.target].Distance(npc.Center)<50f);
+            bool move = (Main.player[npc.target].Distance(npc.Center)<500f);
 
             if (move)
             {
-                if (npc.netID == NetmodeID.Server)
+                if (Main.netMode!=1)
                 {
-                    //float random;
+                    npc.velocity.X = (((float)generator.Next(0, 3) - 1) * 10f);
+                    npc.velocity.Y = (((float)generator.Next(0, 3) - 1) * 10f);
                 }
             }
 
             //end of constant movement, attack orb functions start soon
 
-            if (AI_Attack_Element1!=0)
-            {
+            //if (AI_Attack_Element1!=0)
+            //{
                 //enable attacks of orb/element 1 type
 
-            }
+            //}
 
         }
 
