a3d849e4b16c0a5f08231916005fdafc77cdea0f
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-09-27 22:24:55 -0500

    Added basic generic orb code

---
+++ NPCs/Prismachine/Orb.cs
@@ -0,0 +1,50 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace prismmod.NPCs.Prismachine
+{
+    class Orb : ModNPC
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Orb");
+            Main.npcFrameCount[npc.type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            //update hitbox to match sprite: KEEP HITBOX OUT OF ORB HITBOXES
+            npc.width = 25;
+            npc.height = 25;
+            //no Ai Style
+            npc.lifeMax = 100; //ask braden for life values
+            //beware of generic hit sounds
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            //?npc.value = 0.75f;
+            npc.knockBackResist = 1f;
+            npc.damage = 1000; //lots of damage, should not get close to it, bceeause it should not get close to you
+            // npc.immune = false;
+            npc.noGravity = true;
+            npc.noTileCollide = true;
+        }
+        
+        
+        public override void AI()
+        {
+            NPC prismachine = Main.npc[(int)npc.ai[0]];
+        }
+
+        public override void NPCLoot()
+        {
+            NPC prismachine = Main.npc[(int)npc.ai[0]];
+            prismachine.ai[0] = 1;
+        }
+
+
+    }
+}
