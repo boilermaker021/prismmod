17ae3766fa1582bab3927089ed7d48fde7978c84
Nathan Leakeas <nathan.leakeas@gmail.com>
2019-09-29 22:22:15 -0500

    Create PrismachineSpike.cs

---
+++ NPCs/Prismachine/PrismachineSpike.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace prismmod.Projectiles
+{
+    public class PrismachineSpike : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Prismachine Spike");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.friendly = false;
+            projectile.ranged = true;
+        }
+        public override void AI()
+        {
+            projectile.velocity.Y = projectile.velocity.Y + 0.3f;
+        }
+
+    }
+}
