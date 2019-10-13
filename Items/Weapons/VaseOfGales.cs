af009a129a5a296fff46bbb3f9c715e0dca50c22
manamaster <34355662+manamster@users.noreply.github.com>
2019-10-13 11:23:18 -0500

    started work on vase of gales
    doesnt do much right now need to change it to magic weapon and also add the vaccum feature in.

---
+++ Items/Weapons/VaseOfGales.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace prismmod.Items.Weapons
+{
+    public class VaseOfGales : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Vase Of Gales");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 20;
+            item.ranged = false;
+            item.width = 33;
+            item.height = 22;
+            item.useTime = 0;
+            item.useAnimation = 30;
+            item.useStyle = 5;
+            item.noMelee = false;
+            item.rare = 2;
+            item.UseSound = SoundID.Item33;
+            item.autoReuse = true;
+            item.rare = -12;
+            item.scale = 0.75f;
+        }
+
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-8, 0);
+        }
+    }
+}
