437522d2972ce413baf8c8b083720ec78eb7ced8
GeneralKenobi <Personman48759@yahoo.com>
2019-10-13 18:26:32 -0500

    Coded most of Boulduck Sword

--- Items/Weapons/BoulducksRetribution.cs
+++ Items/Weapons/BoulducksRetribution.cs
@@ -1,15 +1,17 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
 
 namespace prismmod.Items.Weapons
 {
-    public class TrappingEdge : ModItem
+    public class BoulducksRetribution : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Trapping Edge");
-            Tooltip.SetDefault("Freezes enemies while they are being stabbed.");
+            DisplayName.SetDefault("Boulducks Retribution");
+            Tooltip.SetDefault("Quack.");
         }
 
         public override void SetDefaults()
@@ -20,29 +22,17 @@ namespace prismmod.Items.Weapons
             item.height = 40;
             item.useTime = 20;
             item.useAnimation = 10;
-            item.useStyle = 3;
+            item.useStyle = 1;
             item.knockBack = 0;
             item.value = 10000;
             item.rare = 2;
-            item.UseSound = SoundID.Item1;
+            item.shoot = mod.ProjectileType("BoulducksRetributionProjectile");
+            item.shootSpeed = 7f;
+            item.UseSound = new Terraria.Audio.LegacySoundStyle(SoundID.Duck, 0);
             item.autoReuse = false;
         }
 
-        public override void OnHitNPC(Player player, NPC target, int damage, float knoclkback, bool crit)
-        {
-            target.AddBuff(mod.BuffType("Freezing"), 60);
-        }
 
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.CopperShortsword, 1);
-            recipe.AddIngredient(ItemID.Stinger, 1);
-            recipe.AddIngredient(ItemID.Wood, 1);
-            recipe.AddIngredient(ItemID.PlatinumBar, 3);
-            recipe.AddTile(TileID.WorkBenches);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
-        }
+
     }
 }
