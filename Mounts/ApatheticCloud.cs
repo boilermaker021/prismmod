cd6e7d44e25cfe5c11431b6053cca3e12e135147
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-10-18 20:08:07 +0000

    Created template file for the Apathetic Cloud

---
+++ Mounts/ApatheticCloud.cs
@@ -0,0 +1,23 @@
+using ExampleMod.Buffs;
+using ExampleMod.Dusts;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace prismmod.Mounts
+{
+    public class ApatheticCloud: ModMountData
+    {
+        public override void SetDefaults()
+        {
+
+
+        }
+
+    }
+
+
+}
