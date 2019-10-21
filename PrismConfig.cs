69d01fefca70e3b92c1addbe233efbc3aabd1989
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-10-21 16:48:05 -0500

    Fixed config compile errors

--- PrismConfig.cs
+++ PrismConfig.cs
@@ -1,16 +1,20 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Config;
 using Terraria.ModLoader.Config.UI;
 using Terraria.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 
 namespace prismmod
 {
@@ -19,9 +23,9 @@ namespace prismmod
         public override ConfigScope Mode => ConfigScope.ServerSide;
 
         [Label("Disable Developer items")]
-        [DefaultValue(false)]
         [ReloadRequired]
-        public bool DisableDevItems {get; set;};
+        [DefaultValue(false)]
+        public bool DisableDevItems {get; set;}
 
         public override bool AcceptClientChanges(ModConfig pendingConfig, int whoAmI, ref string message)
         {
