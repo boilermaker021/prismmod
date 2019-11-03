9c99dbc4009bc7b0109e4c6f35dc4384ac37bd37
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-11-02 19:05:34 -0500

    Update PrismWorld.cs

--- PrismWorld.cs
+++ PrismWorld.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using System.IO;
 using Terraria;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.World.Generation;
 
 namespace prismmod
 {
@@ -29,5 +33,47 @@ namespace prismmod
             killedGargantuanTortoise = flags[0];
             downedPrismachine = flags[1];
         }
+
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            /*int genIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Terrain"));
+            if (genIndex != -1)
+            {
+                tasks.Insert(genIndex + 1, new PassLegacy("IRON CUBE", delegate (GenerationProgress progress)
+                {
+                    progress.Message = "CUBE";
+                    int x = (int)Main.spawnTileX;
+                    int y = (int)Main.spawnTileY;
+
+                    for (int i = 0; i < 25; i++)
+                    {
+                        for (int j = 0; j < 25; j++)
+                        {
+                            
+                            WorldGen.PlaceTile(x - i + 1, y - j, TileID.Iron);
+                        }
+                        
+                    }
+
+                    /*for (int i = 0; i < 10; i++)
+                    {
+                        WorldGen.PlaceTile(x, y + i - 30, TileID.Iron);
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        WorldGen.PlaceTile(x + 10, y + i - 30, TileID.Iron);
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        WorldGen.PlaceTile(x + i, y + 10 - 30, TileID.Iron);
+                    }*/
+
+                }));
+            }
+        }
+
+
     }
 }
