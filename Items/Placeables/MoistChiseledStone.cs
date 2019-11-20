8d517042c0b731000f9cbd6364cca30f6cab0b47
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-11-20 17:51:47 -0600

    Created tile and items for Moist Chiseled Stone

---
+++ Items/Placeables/MoistChiseledStone.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace prismmod.Items.Placeables
+{
+    class MoistChiseledStone : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Moist Chiseled Stone");
+            Tooltip.SetDefault("Can be placed");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = ModContent.TileType<Tiles.Blox.MoistChiseledStone>();
+        }
+    }
+}
---
+++ Tiles/Blox/MoistChiseledStone.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace prismmod.Tiles.Blox 
+{
+    public class MoistChiseledStone : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = true;
+            drop = ItemType<Items.Placeables.MoistChiseledStone>();
+            AddMapEntry(new Color(200, 200, 200));
+        }
+ 
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 0.5f;
+            g = 0.5f;
+            b = 0.5f;
+        }
+
+
+    }
+}
