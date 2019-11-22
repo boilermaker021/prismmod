85cee4a054d9fb3df77470d458ff928f50ecd6be
CommieSlayer <nathan.leakeas@gmail.com>
2019-11-22 15:39:27 +0000

    Started creating PrismmodHelper.cs

---
+++ PrismmodHelper.cs
@@ -0,0 +1,17 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace prismmod
+{
+    internal class PrismmodHelper
+    {
+        //Building Section
+        Tile[] waterHouse
+
+        //other values
+        
+
+    }
+
+}
