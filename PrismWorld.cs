99a032a6cebc602b95d1def20b5f604eeeac5f0c
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-11-21 18:25:10 -0600

    Update PrismWorld.cs

--- PrismWorld.cs
+++ PrismWorld.cs
@@ -6,6 +6,8 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.World.Generation;
 
+using prismmod.NPCs.WaterTown;
+
 namespace prismmod
 {
     internal class PrismWorld : ModWorld
@@ -51,7 +53,7 @@ namespace prismmod
             }*/
 
             //@todo fix infinite glass spawning before Smoothing World gen task
-            int genIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Beaches"));
+            int genIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Micro Biomes"));
             if (genIndex != -1)
             {
                 tasks.Insert(genIndex + 1, new PassLegacy("Generate  Water Town", delegate (GenerationProgress progress)
@@ -92,7 +94,7 @@ namespace prismmod
                         {
                             Tile tile = Framing.GetTileSafely(xCoord, yCoord);
                             tile.ClearTile();
-                            if (((xCoord == 59 || xCoord == 119)||(yCoord==Main.spawnTileY+149||yCoord==Main.spawnTileY+120)) && !((xCoord-59<10)&&(yCoord==120)))//last check may screw things up big time
+                            if (((xCoord == 59 || xCoord == 119)||(yCoord==Main.spawnTileY+149||yCoord==Main.spawnTileY+120))&&!(yCoord==Main.spawnTileY+120&&((xCoord-59)<10)&&(xCoord-59)>0))//last check may screw things up big time
                             {
                                 WorldGen.PlaceTile(xCoord, yCoord, activeBlock);
                             }
@@ -126,6 +128,7 @@ namespace prismmod
 
                     //@todo Add fish npcs
                     progress.Message = "Importing Fish People";
+                    NPC.NewNPC((100)*16, (Main.spawnTileY + 130)*16,ModContent.NPCType<FishBlue>());
 
                 }));
             }
