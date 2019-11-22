39689f1d205f4cbc4a10d480797b52298c324d2e
CommieSlayer <nathan.leakeas@gmail.com>
2019-11-22 16:50:04 +0000

    Added exampleSquare

--- PrismmodHelper.cs
+++ PrismmodHelper.cs
@@ -6,8 +6,15 @@ namespace prismmod
 {
     internal class PrismmodHelper
     {
+        Tile mcs = ModContent.TileType<Tiles.Blox.MoistChiseledStone>()
+        string na = "nothing";
+
         //Building Section
-        Tile[] waterHouse
+        Tile[] exampleSquare = {//This array is currently set up to create a hollow 3x3 square of moist chiseled stone
+            {mcs,mcs,mcs},
+            {mcs,na,mcs},
+            {mcs,mcs,mcs},
+        }
 
         //other values
         string[] fishNames = { "Coral","Jerry","Mark","Bubbles","Octavius, Destroyer of Worlds","Reefback", "Gerald", "Markus", "Vincent", "Tom", "Bofa", "Gex", "Salmonelly", "Jeb", "Joel", "[REDACTED]"};
