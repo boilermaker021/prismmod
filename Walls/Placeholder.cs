9b82ff74e24fc60e7f98b5a3817d30b7f721d1dd
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-11-23 09:55:08 -0600

    Create Placeholder.cs

---
+++ Walls/Placeholder.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace prismmod.Walls
+{
+    class Placeholder : ModWall
+    {
+        public override void SetDefaults()
+        {
+            Main.wallHouse[Type] = true;
+            AddMapEntry(new Color(30, 30, 30));
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            base.ModifyLight(i, j, ref r, ref g, ref b);
+        }
+
+        public override bool CanExplode(int i, int j)
+        {
+            return false;
+        }
+
+    }
+}
