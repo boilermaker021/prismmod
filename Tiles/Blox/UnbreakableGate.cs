17fc5c0d0ad6c2d49a2478d9b90693eee61607c4
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-12-22 23:50:45 -0600

    Create UnbreakableGate.cs

---
+++ Tiles/Blox/UnbreakableGate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace prismmod.Tiles.Blox
+{
+    public class UnbreakableGate : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = true;
+            //NO DROPABLES//drop = ItemType<Items.Placeables.ITEM>();
+            AddMapEntry(new Color(74, 71, 245));
+            mineResist = 7f;
+            minPick = 10000;
+            
+        }
+
+        public override bool CanExplode(int i, int j)
+        {
+            return false;
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 74/255;
+            g = 71/255;
+            b = 245/255;
+        }
+    }
+}
