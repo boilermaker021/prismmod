1bdae27d0344df0d41ed20b486af585d3c12211f
CommieSlayer <nathan.leakeas@gmail.com>
2020-03-26 20:20:59 -0500

    Placeholder to change water color

---
+++ Waters/WaterTownWaterStyle.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+
+namespace prismmod.Waters
+{
+	class WaterTownWaterStyle
+	{
+		public class ExampleWaterStyle : ModWaterStyle
+		{
+			public override bool ChooseWaterStyle()
+				=> Main.LocalPlayer.GetModPlayer<PrismPlayer>().ZoneWaterTown;
+
+			public override int ChooseWaterfallStyle()
+				=> mod.GetWaterfallStyleSlot("ExampleWaterfallStyle");
+
+			public override int GetSplashDust()
+				=> DustID.Blood;
+
+			public override int GetDropletGore()
+				=> GoreID.WaterDrip;
+
+			public override void LightColorMultiplier(ref float r, ref float g, ref float b)
+			{
+				r = 1f;
+				g = 1f;
+				b = 1f;
+			}
+		}
+	}
+}
