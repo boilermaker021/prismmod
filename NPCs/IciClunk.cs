cb3d0a5c47d453c82e974d5c5bf407031ccd9c0f
CommieSlayer <nathan.leakeas@gmail.com>
2020-05-17 15:22:07 -0500

    More Iclclunk MP changes

--- NPCs/IciClunk.cs
+++ NPCs/IciClunk.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using System;
 using Terraria;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace prismmod.NPCs
@@ -61,9 +62,8 @@ namespace prismmod.NPCs
         public float AI_Frame
         {
             get => npc.ai[AI_Frame_Slot];
-            set => npc.ai[AI_Frame_Slot] = value; 
+            set => npc.ai[AI_Frame_Slot] = value;
         }
-
 
         public float Rotation
         {
@@ -73,120 +73,113 @@ namespace prismmod.NPCs
 
         public override void AI()
         {
-            if (Main.netMode != 1)
+            if (AI_State == AI_State_Waiting)
             {
-                if (AI_State == AI_State_Waiting)
+                npc.rotation = 0f;
+                jumped = false;
+                hitPlayer = false;
+
+                float adjDistance = (Main.player[npc.target].Center.X - npc.Center.X) * 0.0035f;
+                //float adjDistanceY = Math.Abs((Main.player[npc.target].Center.Y - npc.Center.Y) * 0.001f);
+                npc.TargetClosest();
+                if (npc.HasValidTarget && Main.player[npc.target].Distance(npc.Center) < 500f)
                 {
-                    
-                    npc.rotation = 0f;
-                    jumped = false;
-                    hitPlayer = false;
-
-                    float adjDistance = (Main.player[npc.target].Center.X - npc.Center.X) * 0.0035f;
-                    //float adjDistanceY = Math.Abs((Main.player[npc.target].Center.Y - npc.Center.Y) * 0.001f);
-                    npc.TargetClosest();
-                    if (npc.HasValidTarget && Main.player[npc.target].Distance(npc.Center) < 500f)
-                    {
-                        AI_Timer++;
-                        if (AI_Timer % 35 == 0)
-                        {
-                            npc.velocity = new Vector2(20f * adjDistance, -20f);//*adjDistanceY);
-                            jumped = true;
-                            AI_State = AI_State_Jumped;
-                            AI_Timer = 0;
-                        }
-                        else if (AI_Timer % 30 == 0)
-                        {
-                            AI_Frame = 4;
-                        }
-                        else if (AI_Timer % 20 == 0)
-                        {
-                            AI_Frame = 3;
-                        }
-                        
-                    }
-                    else if (npc.HasValidTarget)
-                    {
-                        AI_Timer++;
-                        AI_Frame = 1;
-                        if (AI_Timer % 20 == 0)
-                        {
-                            jumped = true;
-                            npc.velocity = new Vector2(20f * (adjDistance * Math.Abs(1 / adjDistance)), -20f);//*adjDistanceY);
-                            AI_State = AI_State_Movement;
-                            AI_Timer = 0;
-                        }
-                        
-                        
-                        
-                    }
-                }
-                else if (AI_State == AI_State_Movement)
-                {
-                    AI_Frame = 2;
-                    npc.velocity.Y = npc.velocity.Y + 0.5f;
-
-                    if (npc.velocity.Y > 1f)
-                    {
-                        jumped = false;
-                    }
-
-                    if (npc.velocity.Y < 1f && npc.velocity.Y > -1f && jumped == false)
-                    {
-                        AI_State = AI_State_Landed_PostMove;
-                    }
-                }
-                else if (AI_State == AI_State_Jumped)
-                {
-                    AI_Frame = 5;
-                    npc.velocity.Y = npc.velocity.Y + 0.5f; // 0.1f for arrow gravity, 0.4f for knife gravity
-                    if (npc.velocity.Y > 16f) // This check implements "terminal velocity".
-                    {
-                        npc.velocity.Y = 16f;
-                    }
-                    npc.rotation = npc.velocity.ToRotation() + MathHelper.PiOver2;
-
-                    if (hitPlayer)
-                    {
-                        AI_State = AI_State_Landed_PostMove;
-                        npc.rotation = 0f;
-                        hitPlayer = false;
-                    }
-
-                    if (npc.velocity.Y > 1f)
-                    {
-                        jumped = false;
-                    }
-
-                    if (npc.velocity.Y < 1f && npc.velocity.Y > -1f && jumped == false)
-                    {
-                        AI_State = AI_State_Landed_PostJump;
-                        npc.rotation = MathHelper.Pi;
-                    }
-                }
-                else if (AI_State == AI_State_Landed_PostMove)
-                {
-                    AI_Frame = 0;
                     AI_Timer++;
-                    if (AI_Timer > 120)//waits for 2 seconds before the enemy rights itself
+                    if (AI_Timer % 35 == 0)
                     {
+                        npc.velocity = new Vector2(20f * adjDistance, -20f);//*adjDistanceY);
+                        jumped = true;
+                        AI_State = AI_State_Jumped;
                         AI_Timer = 0;
-                        AI_State = AI_State_Waiting;
+                    }
+                    else if (AI_Timer % 30 == 0)
+                    {
+                        AI_Frame = 4;
+                    }
+                    else if (AI_Timer % 20 == 0)
+                    {
+                        AI_Frame = 3;
                     }
                 }
-                else if (AI_State == AI_State_Landed_PostJump)
+                else if (npc.HasValidTarget)
                 {
-                    if (hitPlayer)
+                    AI_Timer++;
+                    AI_Frame = 1;
+                    if (AI_Timer % 20 == 0)
                     {
-                        AI_State = AI_State_Righting;
-                        npc.rotation = 0f;
-                        hitPlayer = false;
-                        AI_Frame = 0;
+                        jumped = true;
+                        npc.velocity = new Vector2(20f * (adjDistance * Math.Abs(1 / adjDistance)), -20f);//*adjDistanceY);
+                        AI_State = AI_State_Movement;
+                        AI_Timer = 0;
                     }
-                    else
-                    {
-                        AI_Timer++;
-                        if((AI_Frame !=6 && AI_Frame!=7)||AI_Timer%8==0)
+                }
+            }
+            else if (AI_State == AI_State_Movement)
+            {
+                AI_Frame = 2;
+                npc.velocity.Y = npc.velocity.Y + 0.5f;
+
+                if (npc.velocity.Y > 1f)
+                {
+                    jumped = false;
+                }
+
+                if (npc.velocity.Y < 1f && npc.velocity.Y > -1f && jumped == false)
+                {
+                    AI_State = AI_State_Landed_PostMove;
+                }
+            }
+            else if (AI_State == AI_State_Jumped)
+            {
+                AI_Frame = 5;
+                npc.velocity.Y = npc.velocity.Y + 0.5f; // 0.1f for arrow gravity, 0.4f for knife gravity
+                if (npc.velocity.Y > 16f) // This check implements "terminal velocity".
+                {
+                    npc.velocity.Y = 16f;
+                }
+                npc.rotation = npc.velocity.ToRotation() + MathHelper.PiOver2;
+
+                if (hitPlayer)
+                {
+                    AI_State = AI_State_Landed_PostMove;
+                    npc.rotation = 0f;
+                    hitPlayer = false;
+                }
+
+                if (npc.velocity.Y > 1f)
+                {
+                    jumped = false;
+                }
+
+                if (npc.velocity.Y < 1f && npc.velocity.Y > -1f && jumped == false)
+                {
+                    AI_State = AI_State_Landed_PostJump;
+                    npc.rotation = MathHelper.Pi;
+                }
+            }
+            else if (AI_State == AI_State_Landed_PostMove)
+            {
+                AI_Frame = 0;
+                AI_Timer++;
+                if (AI_Timer > 120)//waits for 2 seconds before the enemy rights itself
+                {
+                    AI_Timer = 0;
+                    AI_State = AI_State_Waiting;
+                }
+            }
+            else if (AI_State == AI_State_Landed_PostJump)
+            {
+                if (hitPlayer)
+                {
+                    AI_State = AI_State_Righting;
+                    npc.rotation = 0f;
+                    hitPlayer = false;
+                    AI_Frame = 0;
+                }
+                else
+                {
+                    AI_Timer++;
+                    if ((AI_Frame != 6 && AI_Frame != 7) || AI_Timer % 8 == 0)
                         if (AI_Frame == 6)
                         {
                             AI_Frame = 7;
@@ -199,40 +192,48 @@ namespace prismmod.NPCs
                         {
                             AI_Frame = 6;
                         }
-                        if (AI_Timer > 180)//waits for 2 seconds before the enemy rights itself
-                        {
-                            
-                            AI_Timer = 0;
-                            AI_State = AI_State_Righting;
-                            jumped = true;
-                            npc.velocity.Y = -6f;
-                            npc.velocity.X = -2f;
-                        }
-                        npc.netUpdate = true;
-                    }
-                }
-                else if (AI_State == AI_State_Righting)
-                {
-                    AI_Frame = 0;
-                    npc.rotation = (npc.velocity.ToRotation() + MathHelper.PiOver2) - MathHelper.Pi;
-                    if (npc.velocity.Y > 1f)
+                    if (AI_Timer > 180)//waits for 2 seconds before the enemy rights itself
                     {
-                        jumped = false;
+                        AI_State = AI_State_Righting;
+                        AI_Timer = 0;
+                        jumped = true;
+                        npc.velocity.Y = -6f;
+                        npc.velocity.X = -2f;
                     }
-
-                    if (npc.velocity.Y < 1f && npc.velocity.Y > -1f && jumped == false)
-                    {
-                        AI_State = AI_State_Landed_PostMove;
-                        npc.rotation = 0f;
-                    }
+                    npc.netUpdate = true;
                 }
             }
-            
+            else if (AI_State == AI_State_Righting)
+            {
+                Main.NewText("Righting");
+                AI_Frame = 0;
+                
+                if (npc.velocity.Y > 1f)
+                {
+                    jumped = false;
+                }
+                if (npc.velocity.Y < 1f && npc.velocity.Y > -1f && jumped == false)
+                {
+                    AI_State = AI_State_Landed_PostMove;
+                    npc.rotation = 0f;
+                }
+                else
+                {
+                    npc.rotation = (npc.velocity.ToRotation() + MathHelper.PiOver2) - MathHelper.Pi;
+                }
+            }
         }
 
         public override void OnHitPlayer(Player target, int damage, bool crit)
         {
-            hitPlayer = true;
+            if (AI_State == AI_State_Jumped)
+            {
+                AI_State = AI_State_Righting;
+                //npc.netUpdate = true;
+                NetworkText text = NetworkText.FromLiteral("bruh");
+                Color color = new Color(112, 202, 204);
+                NetMessage.BroadcastChatMessage(text, color);
+            }
         }
 
         public override void FindFrame(int frameHeight)
@@ -248,10 +249,9 @@ namespace prismmod.NPCs
             6.
             7.
             8.
-             
+
             */
             npc.frame.Y = frameHeight * (int)AI_Frame;
-
         }
 
         public override void HitEffect(int hitDirection, double damage)
